0582af093736a750d1447fc02f72ca38368b0a6e
darikmohammed <r.m.darik@gmail.com>
2022-07-12 01:30:19 +0300

    Add redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'react-redux';
+import books from './books/books';
+import categories from './categories/categories';
+
+const rootReducer = combineReducers({
+  books,
+  categories,
+});
+
+export default rootReducer;
