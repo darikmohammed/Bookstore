29d814b46ecf023057d1b9d1ccaa7c13a59aa00c
darikmohammed <r.m.darik@gmail.com>
2022-07-12 01:23:21 +0300

    add actions and reducer for category

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,1 +1,16 @@
-// 
+// ACTION
+const STATUS = 'bookStore/category/status';
+
+// REDUCER
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case STATUS:
+      return ['Comming soon!'];
+    default:
+      return state;
+  }
+};
+// ACTION CREATOR
+
+export const categoryStatus = () => ({ type: STATUS });
