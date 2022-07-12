22df27b57fe0386fe04ec4c260854c859c8959b0
darikmohammed <r.m.darik@gmail.com>
2022-07-13 01:21:22 +0300

    update intial state of books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,28 @@
 // ACTION
 const ADD_BOOK = 'bookStore/books/ADD';
 const REMOVE_BOOK = 'bookStore/books/REMOVE';
-
+const intialState = () => [
+  {
+    Author: 'Suzanne Collins',
+    Title: 'The Hunger Games',
+    Category: 'Action',
+    progress: '64',
+  },
+  {
+    Author: 'Frank Herbert',
+    Title: 'Dune',
+    Category: 'Science Fiction',
+    progress: '8',
+  },
+  {
+    Author: 'Suzanne Collins',
+    Title: 'Capital in the Twenty-First Century',
+    Category: 'Economy',
+    progress: '1',
+  },
+];
 // REDUCER
-export default (state = [], action) => {
+export default (state = intialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
