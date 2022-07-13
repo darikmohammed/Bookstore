df81cf116654e5cc380be456e9fc4a26d9280375
darikmohammed <r.m.darik@gmail.com>
2022-07-13 04:08:23 +0300

    Add functionality for check status button

--- src/Routes/Catagory.js
+++ src/Routes/Catagory.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoryStatus } from '../redux/categories/categories';
 import Nav from '../Components/Nav';
+import './Styles/Category.css';
 
 function Catagory() {
+  const value = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const handleStatusClick = () => {
+    dispatch(categoryStatus());
+  };
   return (
     <div>
       <Nav />
-      <h2 className="category">Comming soon!</h2>
+      <div className="body-container">
+        <button type="button" className="btn" onClick={handleStatusClick}>
+          Check Status
+        </button>
+        <h2 className="category">{value}</h2>
+      </div>
     </div>
   );
 }
