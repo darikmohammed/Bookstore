916dec209087d3c42d4d867aff95b8657db296be
darikmohammed <r.m.darik@gmail.com>
2022-07-13 05:00:05 +0300

    Add component managment for the input fields

--- src/Components/NewBook.js
+++ src/Components/NewBook.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 import './Styles/NewBook.css';
 
 function NewBook() {
+  const [title, setTitle] = useState('');
+  const [autor, setAutor] = useState('');
   const dispatch = useDispatch();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleAutorChange = (e) => {
+    setAutor(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (e.target.elements.autor.value && e.target.elements.title.value) {
       const obj = {
-        Author: e.target.elements.autor.value,
-        Title: e.target.elements.title.value,
+        Author: autor,
+        Title: title,
         Category: 'New Category',
         progress: '1',
       };
       dispatch(addBook(obj));
-      e.target.elements.autor.value = '';
-      e.target.elements.title.value = '';
+      setTitle('');
+      setAutor('');
     }
   };
   return (
@@ -28,12 +37,16 @@ function NewBook() {
           placeholder="Book title"
           className="book-title"
           name="title"
+          value={title}
+          onChange={handleTitleChange}
         />
         <input
           type="text"
           placeholder="Book autor"
           className="book-autor"
           name="autor"
+          value={autor}
+          onChange={handleAutorChange}
         />
         <button type="submit" className="form-btn">
           ADD BOOK
